7bcdeac059d05945c39924c90c898d48eec974c4
boniife <131681425+Boniife@users.noreply.github.com>
2023-10-27 03:33:47 +0100

    update

--- frontend/src/components/constants/constant.js
+++ frontend/src/components/constants/constant.js
@@ -5,14 +5,14 @@ export const Cards = [
     id: '1',
     title: 'Blue Dart',
     descripton: 'Delivery service',
-    link: 'http://link.com',
+    link: 'https://cozy-cheesecake-017fe1.netlify.app',
     img: blue,
   },
   {
     id: '2',
     title: 'Boni youtube',
     descripton: 'youtube-clone',
-    link: 'http://link.com',
+    link: 'https://storied-centaur-8e6c60.netlify.app',
     img: youtube,
   },
 ];

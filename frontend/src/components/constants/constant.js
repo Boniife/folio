d5cc74ceb8eb69dde6359f79786d6472caee9ef4
Boniife <anyanwuifeanyi3@gmail.com>
2023-12-23 17:32:07 +0100

    new project added

--- frontend/src/components/constants/constant.js
+++ frontend/src/components/constants/constant.js
@@ -1,35 +1,43 @@
-import youtube from '../../assets/images/youtube.png';
-import blue from '../../assets/images/blue.png';
-import fit from '../../assets/images/fit.png';
-import travel from '../../assets/images/travel.png';
+import youtube from "../../assets/images/youtube.png";
+import blue from "../../assets/images/blue.png";
+import fit from "../../assets/images/fit.png";
+import travel from "../../assets/images/travel.png";
+import cars from "../../assets/images/cars.png";
 
 export const Cards = [
   {
-    id: '1',
-    title: 'Blue Dart',
-    descripton: 'Delivery service',
-    link: 'https://cozy-cheesecake-017fe1.netlify.app',
+    id: "1",
+    title: "Blue Dart",
+    descripton: "Delivery service",
+    link: "https://cozy-cheesecake-017fe1.netlify.app",
     img: blue,
   },
   {
-    id: '2',
-    title: 'Boni youtube',
-    descripton: 'youtube-clone',
-    link: 'https://storied-centaur-8e6c60.netlify.app',
+    id: "2",
+    title: "Boni youtube",
+    descripton: "youtube-clone",
+    link: "https://storied-centaur-8e6c60.netlify.app",
     img: youtube,
   },
   {
-    id: '3',
-    title: 'Boni-Fit',
-    descripton: 'Fitness Application',
-    link: 'https://clinquant-stardust-ee5c74.netlify.app',
+    id: "3",
+    title: "Boni-Fit",
+    descripton: "Fitness Application",
+    link: "https://clinquant-stardust-ee5c74.netlify.app",
     img: fit,
   },
   {
-    id: '4',
-    title: 'Boni-Travels',
-    descripton: 'Travel agency',
-    link: 'https://boni-travels.vercel.app',
+    id: "4",
+    title: "Boni-Travels",
+    descripton: "Travel agency",
+    link: "https://boni-travels.vercel.app",
     img: travel,
+  },
+  {
+    id: "5",
+    title: "Boni-Car-ShowCase",
+    descripton: "Car ShowCase",
+    link: "https://boni-cars.vercel.app/",
+    img: cars,
   },
 ];
